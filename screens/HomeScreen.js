b36e68482d36f7ef86e612eebb3749c526d76656
Jean Pierre Velez <vcjpierre@gmail.com>
2020-10-26 13:52:51 -0400

    Use styled components in home  screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,26 +1,14 @@
 import React, { useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import { Container } from '../styles/FeedStyles'
+
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text style={styles.text}>Welcome</Text>
-    </View>
+    </Container>
   );
 }
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f9fafd',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  text: {
-    fontSize: 20,
-    color: '#333333'
-  }
-});
