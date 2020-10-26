7497bb0f551dffb1bed35e1cdcf6f679412b9cb0
Jean Pierre Velez <vcjpierre@gmail.com>
2020-10-26 00:38:12 -0400

    Add tabs

--- navigation/AppStack.js
+++ navigation/AppStack.js
@@ -1,14 +1,109 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import HomeScreen from '../screens/HomeScreen';
+import AddPostScreen from '../screens/AddPostScreen';
+import ChatScreen from '../screens/ChatScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const FeedStack = ({navigation}) => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Social App"
+      component={HomeScreen}
+      options={{
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: '#2e64e5',
+          fontFamily: 'Kufam-SemiBoldItalic',
+          fontSize: 18,
+        },
+        headerStyle: {
+          shadowColor: '#fff',
+          elevation: 0,
+        },
+        headerRight: () => (
+          <View style={{marginRight: 10}}>
+            <FontAwesome5.Button
+              name="plus"
+              size={22}
+              background="#fff"
+              color="#2e64e5"
+              onPress={() => navigation.navigate('AddPost')}
+            />
+          </View>
+        ),
+      }}
+    />
+    <Stack.Screen
+      name="AddPost"
+      component={AddPostScreen}
+      options={{
+        title: '',
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#2e64e515',
+          shadowColor: '#fff',
+          elevation: 0,
+        },
+        headerBackTitleVisible: false,
+        headerBackImage: () => (
+          <View style={{marginLeft:15}}>
+
+          </View>
+        )
+      }}
+    />
+  </Stack.Navigator>
+);
 
 const AppStack = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#2e64e5',
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={FeedStack}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => (
+            <MaterialCommunityIcons
+              name="home-outline"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={ChatScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="chat-ellipses-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name="chat-ellipses-outline" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
